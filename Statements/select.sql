97522e4d4969a37650067b31594523ce0a37ae04
Joseph Angel Anthony Garza Lopez <joseph88garza@gmail.com>
2025-11-16 00:21:22 -0600

    comandos select

--- Statements/select.sql
+++ Statements/select.sql
@@ -1,1 +1,6 @@
-SELECT * FROM users;
+SELECT * FROM tableName; /* Consultar datos de toda la tabla */
+
+SELECT ColumName from tableName; /*Consultar datos sobre una columna especifica */
+
+SELECT user_id, name FROM user; /*Consultar datoss sobre varias columnas */
+
