81925a2e39665837a76121c2563c04d7ae330a29
Joseph Angel Anthony Garza Lopez <joseph88garza@gmail.com>
2025-11-16 00:47:51 -0600

    disntict sql

---
+++ Statements/02_distinct.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT * FROM users; /* Consultar datos distintos de toda la tabla */
+
+SELECT DISTINCT columnName FROM users; /* Consultar datos distintos de una tabla */ 
+
+SELECT DISTINCT columnName, columnName FROM users; /* Consultar varios datos distintos de una tabla */
