7e5c0357dce8731a8148f3860379757b56c9ebf6
Joseph Angel Anthony Garza Lopez <joseph88garza@gmail.com>
2025-11-19 11:36:38 -0600

    group_by_1/2

--- Statements/17_group_by.sql
+++ Statements/17_group_by.sql
@@ -1,2 +1,6 @@
---GROUP BY
---
+-- GROUP BY
+-- Calcula el valor máximo de la columna para cada grupo único de valores en la propia columna 'age'
+SELECT MAX(columnName) FROM tableName GROUP BY columnName;
+
+-- Cuenta cuántos usuarios hay para cada valor único en la columna (agrupando por edad)
+SELECT COUNT(columnName), columnName FROM tableName GROUP BY columnName;
